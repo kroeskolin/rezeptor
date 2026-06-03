c16fefeb728a16fe7861d19ffdb1996b4598f018
kroeskolin <karolin.kroeske@yahoo.de>
2026-06-03 20:48:27 +0200

    add deploy workflow

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,8 @@ import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
 import basicSsl from '@vitejs/plugin-basic-ssl'
 
-
 export default defineConfig({
+  base: '/rezeptor/',
   plugins: [
     basicSsl(),
     react(),
